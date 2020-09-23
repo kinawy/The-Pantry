de5a8dd72188b202edda8dd5d9b1f14414a4630b
kinawy <sameh.yasser.kinawy@gmail.com>
2020-09-23 16:26:02 -0400

    Adds base.html, adds search.html, adds views and urls

--- thepantry/thepantry/urls.py
+++ thepantry/thepantry/urls.py
@@ -18,4 +18,8 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
+    path('profile/', views.profile, name='profile')
+
 ]
